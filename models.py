51eb946590f8d94cdf28640dcf8d22ae42144dd5
Jason van Lint <jvanlint@mac.com>
2021-04-07 16:46:09 +1000

    Support object added to model

--- models.py
+++ models.py
@@ -287,3 +287,32 @@ class Threat(models.Model):
         max_length=10, choices=THREAT_TYPES, null=True)
     description = models.TextField(
         help_text='Enter Threat Description/Situation.', default="Threat description to be added here.")
+
+
+class Support(models.Model):
+
+    # Fields
+
+    mission = models.ForeignKey(
+        'Mission', on_delete=models.CASCADE, null=True)
+
+    THREAT_TYPES = (
+        ('AWACS', 'AWACS'),
+        ('TANKER', 'TANKER'),
+        ('JTAC', 'JTAC'),
+        ('CARRIER', 'CARRIER'),
+
+    )
+
+    callsign = models.CharField(max_length=50)
+    support_type = models.CharField(
+        max_length=10, choices=THREAT_TYPES, null=True)
+    frequency = models.CharField(max_length=10, null=True, blank=True)
+    tacan = models.CharField(max_length=10, null=True, blank=True)
+    altitude = models.CharField(max_length=10, null=True, blank=True)
+    speed = models.CharField(max_length=10, null=True, blank=True)
+    brc = models.CharField(max_length=10, null=True, blank=True)
+    icls = models.CharField(max_length=10, null=True, blank=True)
+
+    notes = models.TextField(
+        help_text='Enter notes for support resource.', null=True, blank=True)
