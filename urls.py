9103814adadc108ab3f4091150302595c592d25a
Jason van Lint <jvanlint@mac.com>
2021-05-12 22:49:06 +1000

    Reset password code complete and templates added.

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -77,6 +79,15 @@ urlpatterns = [
     path("logout", views.logout_request, name="logout"),
 
     path("changepassword", views.change_password, name="changepassword"),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_sent.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token> ', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_form.html'),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_complete'),
 
     path('pdf_view/mission/<int:mission_id>/flight/<int:flight_id>',
          views.view_mission_card, name="pdf_view"),
