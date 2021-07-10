93e1e68163fef59b2d878e763b44aacab86f1d3e
Jason van Lint <jvanlint@mac.com>
2021-07-10 19:35:37 +1000

    Added flight coordination field to Flight model.

--- models.py
+++ models.py
@@ -286,6 +286,9 @@ class Flight(models.Model):
         max_length=200, help_text='Enter Flight Callsign')
     task = models.ForeignKey(
         'Task', on_delete=models.CASCADE, null=True)
+    flight_coordination = models.TextField(
+        help_text='Use this field to enter in any notes that the flight lead might need to use to coordinate other members of the flight,', null=True, blank=True,
+        verbose_name="Notes for flight co-ordination")
     radio_frequency = models.CharField(
         max_length=20, help_text='Enter Flight Frequency', blank=True, null=True)
     tacan = models.CharField(
