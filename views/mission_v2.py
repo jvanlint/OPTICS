c9ae8ac11ca3815896c3639a090ad62dee999b64
Steve <github@ausaviators.com>
2021-08-09 13:14:52 +1000

    Mission_v2 load file missing

--- views/mission_v2.py
+++ views/mission_v2.py
@@ -14,127 +14,146 @@ from ..forms import MissionForm
 
 # ---------------- Mission -------------------------
 
-@login_required(login_url='login')
+
+@login_required(login_url="login")
 def mission_v2(request, link_id):
-	mission_queryset = Mission.objects.get(id=link_id)
-	mission_files_queryset = mission_queryset.missionfile_set.all()
-	combat_files_queryset = mission_queryset.combatflitefile_set.all()
-	comments = mission_queryset.comments.all()
-	packages = mission_queryset.package_set.all()
-	targets = mission_queryset.target_set.all()
-	threats = mission_queryset.threat_set.all()
-	supports = mission_queryset.support_set.all()
-	user_profile = UserProfile.objects.get(user=request.user)
-	
-	breadcrumbs = {'Home': reverse('campaigns'),  mission_queryset.campaign.name: reverse('campaign_detail_v2', args=(mission_queryset.campaign.id,)), mission_queryset.name:''}
- 
-	context = {
-			   'mission_object': mission_queryset,
-			   'package_object': packages,
-			   'target_object': targets,
-			   'threat_object': threats,
-			   'support_object': supports,
-			   'mission_files': mission_files_queryset,
-			   'combat_files': combat_files_queryset,
-			   'isAdmin': user_profile.is_admin(), 
-			   'comments': comments,
-			   'breadcrumbs': breadcrumbs,
-			   }
+    mission_queryset = Mission.objects.get(id=link_id)
+    mission_files_queryset = mission_queryset.missionfile_set.all()
+    combat_files_queryset = mission_queryset.combatflitefile_set.all()
+    comments = mission_queryset.comments.all()
+    packages = mission_queryset.package_set.all()
+    targets = mission_queryset.target_set.all()
+    threats = mission_queryset.threat_set.all()
+    supports = mission_queryset.support_set.all()
+    user_profile = UserProfile.objects.get(user=request.user)
 
-	return render(request, 
-				  'v2/mission/mission.html', 
-				  context=context)
+    breadcrumbs = {
+        "Home": reverse("campaigns"),
+        mission_queryset.campaign.name: reverse(
+            "campaign_detail_v2", args=(mission_queryset.campaign.id,)
+        ),
+        mission_queryset.name: "",
+    }
 
-@login_required(login_url='login')
+    context = {
+        "mission_object": mission_queryset,
+        "package_object": packages,
+        "target_object": targets,
+        "threat_object": threats,
+        "support_object": supports,
+        "mission_files": mission_files_queryset,
+        "combat_files": combat_files_queryset,
+        "isAdmin": user_profile.is_admin(),
+        "comments": comments,
+        "breadcrumbs": breadcrumbs,
+    }
+
+    return render(request, "v2/mission/mission.html", context=context)
+
+
+@login_required(login_url="login")
 def mission_add_v2(request, link_id):
-	campaign = Campaign.objects.get(id=link_id)
-	missionCount = campaign.mission_set.count() + 1
-	returnURL = request.GET.get('returnUrl')
+    campaign = Campaign.objects.get(id=link_id)
+    missionCount = campaign.mission_set.count() + 1
+    returnURL = request.GET.get("returnUrl")
 
-	form_title = 'Mission'
+    form_title = "Mission"
 
-	form = MissionForm(initial={'campaign': campaign, 'number': missionCount, 'creator': request.user.id})
+    form = MissionForm(
+        initial={
+            "campaign": campaign,
+            "number": missionCount,
+            "creator": request.user.id,
+        }
+    )
 
-	if request.method == "POST":
-		form = MissionForm(request.POST, request.FILES)
-		if form.is_valid():
-			tmp = form.save(commit=False)
-			tmp.creator = request.user
-			tmp.save()
-			return HttpResponseRedirect(returnURL)
+    if request.method == "POST":
+        form = MissionForm(request.POST, request.FILES)
+        if form.is_valid():
+            tmp = form.save(commit=False)
+            tmp.creator = request.user
+            tmp.save()
+            return HttpResponseRedirect(returnURL)
 
-	context = {'form': form, 'form_title': form_title,
-			   'link': link_id, 'returnURL': returnURL}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def mission_update_v2(request, link_id):
-	mission = Mission.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
-	
-	image_url = request.build_absolute_uri(mission.campaign.thumbnail_image.url)
-	
-	mission.create_discord_event(image_url)
-	
-	
+    mission = Mission.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
 
-	form_title = 'Mission'
+    image_url = request.build_absolute_uri(mission.campaign.thumbnail_image.url)
 
-	form = MissionForm(instance=mission)
+    mission.create_discord_event(image_url)
 
-	if request.method == "POST":
-		form = MissionForm(request.POST, request.FILES, instance=mission)
-		print(request.path)
-		if form.is_valid():
-			form.save(commit=True)
-			return HttpResponseRedirect(returnURL)
+    form_title = "Mission"
 
-	context = {'form': form, 'form_title': form_title,
-			   'link': link_id, 'returnURL': returnURL}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
+    form = MissionForm(instance=mission)
 
-@login_required(login_url='login')
+    if request.method == "POST":
+        form = MissionForm(request.POST, request.FILES, instance=mission)
+        print(request.path)
+        if form.is_valid():
+            form.save(commit=True)
+            return HttpResponseRedirect(returnURL)
+
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
+
+
+@login_required(login_url="login")
 def mission_delete_v2(request, link_id):
-	mission = Mission.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
-	
-	mission_files = mission.missionfile_set.all()
-	
-	#Check to see if an AO Image exists.
-	if mission_files:
-		for file in mission_files:
-			os.remove(os.path.join(settings.MEDIA_ROOT,  str(file.mission_file)))
-	
-	mission.delete()
-	
-	return HttpResponseRedirect(returnURL)
+    mission = Mission.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
+
+    mission_files = mission.missionfile_set.all()
+
+    # Check to see if an AO Image exists.
+    if mission_files:
+        for file in mission_files:
+            os.remove(os.path.join(settings.MEDIA_ROOT, str(file.mission_file)))
+
+    mission.delete()
+
+    return HttpResponseRedirect(returnURL)
+
 
 def mission_add_comment(request):
-	# if this is a POST request we need to process the form data
-	mission_id = request.GET.get('mission_id')
-	returnURL = request.GET.get('returnUrl')
+    # if this is a POST request we need to process the form data
+    mission_id = request.GET.get("mission_id")
+    returnURL = request.GET.get("returnUrl")
 
-	if request.method == 'POST':
-		comment_data = request.POST.dict()
-		comment = comment_data.get("comment_text")
-		# Get the post object
-		mission_object = Mission.objects.get(pk=mission_id)
-		mission_object.comments.create(comment=comment, 
-								 user=request.user)
-		
-	return HttpResponseRedirect(returnURL)
+    if request.method == "POST":
+        comment_data = request.POST.dict()
+        comment = comment_data.get("comment_text")
+        # Get the post object
+        mission_object = Mission.objects.get(pk=mission_id)
+        mission_object.comments.create(comment=comment, user=request.user)
 
-@login_required(login_url='login')
+    return HttpResponseRedirect(returnURL)
+
+
+@login_required(login_url="login")
 def mission_file_delete(request, link_id):
-	mission_file_obj = MissionFile.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
-	
-	
-	#Check to see if an AO Image exists.
-	if mission_file_obj:
-		os.remove(os.path.join(settings.MEDIA_ROOT,  str(mission_file_obj.mission_file)))
-	
-	mission_file_obj.delete()
-	
-	return HttpResponseRedirect(returnURL)
+    mission_file_obj = MissionFile.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
+
+    # Check to see if an AO Image exists.
+    if mission_file_obj:
+        os.remove(os.path.join(settings.MEDIA_ROOT, str(mission_file_obj.mission_file)))
+
+    mission_file_obj.delete()
+
+    return HttpResponseRedirect(returnURL)
