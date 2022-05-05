ba9fe99ca6e96a56c68136b309c1ee2bc525bc0b
Steve <github@ausaviators.com>
2022-05-05 17:03:15 +1000

    Added mission upload start to packages.html

--- models/Comment.py
+++ models/Comment.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 # Required for Generic Keys for Comments
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
@@ -7,14 +8,14 @@ from django.contrib.auth.models import User
 
 class Comment(models.Model):
 
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	comment = models.TextField()
-	date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
 
-	# Below the mandatory fields for generic relation
-	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-	object_id = models.PositiveIntegerField()
-	content_object = GenericForeignKey()
+    # Below the mandatory fields for generic relation
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
-	class Meta:
-		ordering = ['-date_created']
+    class Meta:
+        ordering = ['-date_created']
--- views/ref_tables.py
+++ views/ref_tables.py
@@ -27,9 +27,9 @@ from ..forms import (
 )
 
 
-@login_required(login_url="login")
+@login_required(login_url="account_login")
 def reference_tables(request):
-    
+
     airframe = Airframe.objects.order_by("name")  # Always update the airframe context
     terrain = Terrain.objects.order_by("name")
     status = Status.objects.order_by("name")
@@ -39,14 +39,24 @@ def reference_tables(request):
     threat_type = ThreatType.objects.order_by("name")
 
     page_num = request.GET.get("page", 1)
-    waypoint_paginated = Paginator(object_list=waypoint_type, per_page=5).get_page(page_num)
+    waypoint_paginated = Paginator(object_list=waypoint_type, per_page=5).get_page(
+        page_num
+    )
     airframe_paginated = Paginator(object_list=airframe, per_page=5).get_page(page_num)
-    flight_task_paginated = Paginator(object_list=flight_task, per_page=5).get_page(page_num)
-    campaign_status_paginated = Paginator(object_list=status, per_page=5).get_page(page_num)
+    flight_task_paginated = Paginator(object_list=flight_task, per_page=5).get_page(
+        page_num
+    )
+    campaign_status_paginated = Paginator(object_list=status, per_page=5).get_page(
+        page_num
+    )
     terrain_paginated = Paginator(object_list=terrain, per_page=5).get_page(page_num)
-    support_type_paginated = Paginator(object_list=support_type, per_page=5).get_page(page_num)
-    threat_type_paginated = Paginator(object_list=threat_type, per_page=5).get_page(page_num)
-    
+    support_type_paginated = Paginator(object_list=support_type, per_page=5).get_page(
+        page_num
+    )
+    threat_type_paginated = Paginator(object_list=threat_type, per_page=5).get_page(
+        page_num
+    )
+
     breadcrumbs = {"Home": reverse("index"), "Reference Tables": ""}
 
     template = "v2/reference/reference_tables.html"
@@ -60,78 +70,96 @@ def reference_tables(request):
         "airframe_object": airframe_paginated,
         "breadcrumbs": breadcrumbs,
     }
-    
+
     return render(request, template_name=template, context=context)
+
 
 def waypoint_type_page_manager(request):
     waypoint_type = WaypointType.objects.order_by("name")
     page_num = request.GET.get("page", 1)
-    waypoint_paginated = Paginator(object_list=waypoint_type, per_page=5).get_page(page_num)
-    
+    waypoint_paginated = Paginator(object_list=waypoint_type, per_page=5).get_page(
+        page_num
+    )
+
     template = "v2/reference/includes/waypoint_types.html"
     context = {"waypoint_type_object": waypoint_paginated}
-    
+
     return render(request, template_name=template, context=context)
-    
+
+
 def airframe_page_manager(request):
     airframe = Airframe.objects.order_by("name")
     page_num = request.GET.get("page", 1)
     airframe_paginated = Paginator(object_list=airframe, per_page=5).get_page(page_num)
-    
+
     template = "v2/reference/includes/airframes.html"
     context = {"airframe_object": airframe_paginated}
-    
+
     return render(request, template_name=template, context=context)
+
 
 def flight_task_page_manager(request):
     flight_task = Task.objects.order_by("name")
     page_num = request.GET.get("page", 1)
-    flight_task_paginated = Paginator(object_list=flight_task, per_page=5).get_page(page_num)
-    
+    flight_task_paginated = Paginator(object_list=flight_task, per_page=5).get_page(
+        page_num
+    )
+
     template = "v2/reference/includes/flight_tasks.html"
     context = {"flight_task_object": flight_task_paginated}
-    
+
     return render(request, template_name=template, context=context)
-    
+
+
 def campaign_status_page_manager(request):
     campaign_status = Status.objects.order_by("name")
     page_num = request.GET.get("page", 1)
-    campaign_status_paginated = Paginator(object_list=campaign_status, per_page=5).get_page(page_num)
-    
+    campaign_status_paginated = Paginator(
+        object_list=campaign_status, per_page=5
+    ).get_page(page_num)
+
     template = "v2/reference/includes/campaign_status.html"
     context = {"status_object": campaign_status_paginated}
-    
+
     return render(request, template_name=template, context=context)
+
 
 def terrain_page_manager(request):
     terrain = Terrain.objects.order_by("name")
     page_num = request.GET.get("page", 1)
     terrain_paginated = Paginator(object_list=terrain, per_page=5).get_page(page_num)
-    
+
     template = "v2/reference/includes/terrain.html"
     context = {"terrain_object": terrain_paginated}
-    
+
     return render(request, template_name=template, context=context)
+
 
 def support_type_page_manager(request):
     support_type = SupportType.objects.order_by("name")
     page_num = request.GET.get("page", 1)
-    support_type_paginated = Paginator(object_list=support_type, per_page=5).get_page(page_num)
-    
+    support_type_paginated = Paginator(object_list=support_type, per_page=5).get_page(
+        page_num
+    )
+
     template = "v2/reference/includes/support_types.html"
     context = {"support_type_object": support_type_paginated}
-    
+
     return render(request, template_name=template, context=context)
+
 
 def threat_type_page_manager(request):
     threat_type = ThreatType.objects.order_by("name")
     page_num = request.GET.get("page", 1)
-    threat_type_paginated = Paginator(object_list=threat_type, per_page=5).get_page(page_num)
-    
+    threat_type_paginated = Paginator(object_list=threat_type, per_page=5).get_page(
+        page_num
+    )
+
     template = "v2/reference/includes/threat_types.html"
     context = {"threat_type_object": threat_type_paginated}
-    
+
     return render(request, template_name=template, context=context)
+
 
 def evaluate_reference_object(table, link_id):
     if table == "status":
@@ -167,7 +195,7 @@ def evaluate_reference_form(table):
         return AirframeForm
 
 
-@login_required(login_url="login")
+@login_required(login_url="account_login")
 def reference_object_add(request, table):
     breadcrumbs = {
         "Home": reverse("home"),
@@ -200,7 +228,7 @@ def reference_object_add(request, table):
     return render(request, "v2/generic/data_entry_form.html", context)
 
 
-@login_required(login_url="login")
+@login_required(login_url="account_login")
 def reference_object_update(request, link_id, table):
     refobj = evaluate_reference_object(table, link_id)
     obj = refobj.objects.get(id=link_id)
@@ -232,7 +260,7 @@ def reference_object_update(request, link_id, table):
     return render(request, "v2/generic/data_entry_form.html", context=context)
 
 
-@login_required(login_url="login")
+@login_required(login_url="account_login")
 def reference_object_delete(request, link_id, table):
     refobj = evaluate_reference_object(table, link_id)
     obj = refobj.objects.get(id=link_id)
