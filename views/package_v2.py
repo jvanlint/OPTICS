1ecd8aee304cc60ef6bac5d5261c6c85596af157
Steve <github@ausaviators.com>
2022-08-12 14:01:04 +1000

    Check for null campaign in package breadcrumbs.

--- views/package_v2.py
+++ views/package_v2.py
@@ -9,213 +9,245 @@ import requests
 from ..models import Package, Mission, PackageImagery, UserProfile, Comment
 from ..forms import PackageForm, PackageImageryForm
 
+
 @login_required(login_url="account_login")
 def package_v2(request, link_id):
-	package = Package.objects.get(id=link_id)
-	flights = package.flight_set.all()
-	comments = package.comments.all()
-	user_profile = UserProfile.objects.get(user=request.user)
-	imagery = package.packageimagery_set.all()
-	
-	breadcrumbs = {'Home': reverse('campaigns'),  package.mission.campaign.name: reverse('campaign_detail_v2', args=(package.mission.campaign.id,)), package.mission.name: reverse('mission_v2', args=(package.mission.id,)), package.name:''}
- 
-	context = {
-			   'package_object': package,
-			   'flight_object': flights,
-			   "imagery_object": imagery,
-			   "comments": comments,
-			   "isAdmin": user_profile.is_admin(),
-			   'breadcrumbs': breadcrumbs,
-			   }
+    package = Package.objects.get(id=link_id)
+    flights = package.flight_set.all()
+    comments = package.comments.all()
+    user_profile = UserProfile.objects.get(user=request.user)
+    imagery = package.packageimagery_set.all()
 
-	return render(request, 
-				  'v2/package/package.html', 
-				  context=context)
+    if (
+        package.mission.campaign
+    ):  # fixes error in esting where package might not be connected to a campaign.
+        breadcrumbs = {
+            "Home": reverse("campaigns"),
+            package.mission.campaign.name: reverse(
+                "campaign_detail_v2", args=(package.mission.campaign.id,)
+            ),
+            package.mission.name: reverse("mission_v2", args=(package.mission.id,)),
+            package.name: "",
+        }
+    else:
+        breadcrumbs = {}
+
+    context = {
+        "package_object": package,
+        "flight_object": flights,
+        "imagery_object": imagery,
+        "comments": comments,
+        "isAdmin": user_profile.is_admin(),
+        "breadcrumbs": breadcrumbs,
+    }
+
+    return render(request, "v2/package/package.html", context=context)
+
 
 @login_required(login_url="account_login")
 def package_add_v2(request, link_id):
-	mission = Mission.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
+    mission = Mission.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
 
-	form_title = 'Package'
+    form_title = "Package"
 
-	form = PackageForm(initial={'mission': mission})
+    form = PackageForm(initial={"mission": mission})
 
-	if request.method == "POST":
-		form = PackageForm(request.POST, request.FILES)
-		if form.is_valid():
-			obj=form.save(commit=False)
-			obj.modified_by = request.user
-			obj.created_by = request.user
-			obj.save()
-			return HttpResponseRedirect(returnURL)
+    if request.method == "POST":
+        form = PackageForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.modified_by = request.user
+            obj.created_by = request.user
+            obj.save()
+            return HttpResponseRedirect(returnURL)
 
-	context = {'form': form, 'form_title': form_title,
-			   'link': link_id, 'returnURL': returnURL}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
+
 
 @login_required(login_url="account_login")
 def package_update_v2(request, link_id):
-	package = Package.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
-	
-	form_title = 'Package'
+    package = Package.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
 
-	form = PackageForm(instance=package)
+    form_title = "Package"
 
-	if request.method == "POST":
-		form = PackageForm(request.POST, instance=package)
-		print(request.path)
-		if form.is_valid():
-			obj=form.save(commit=False)
-			obj.modified_by = request.user
-			obj.save()
-			return HttpResponseRedirect(returnURL)
+    form = PackageForm(instance=package)
 
-	context = {'form': form, 'form_title': form_title,
-			   'link': link_id, 'returnURL': returnURL}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
-	
+    if request.method == "POST":
+        form = PackageForm(request.POST, instance=package)
+        print(request.path)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.modified_by = request.user
+            obj.save()
+            return HttpResponseRedirect(returnURL)
+
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
+
+
 @login_required(login_url="account_login")
 def package_delete_v2(request, link_id):
-	package = Package.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
-	
-	package.delete()
-	
-	return HttpResponseRedirect(returnURL)
-	
+    package = Package.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
+
+    package.delete()
+
+    return HttpResponseRedirect(returnURL)
+
+
 # ---------------- Package Comments -------------------------
+
 
 @login_required(login_url="account_login")
 def package_add_comment(request):
-	# if this is a POST request we need to process the form data
-	package_id = request.GET.get("package_id")
+    # if this is a POST request we need to process the form data
+    package_id = request.GET.get("package_id")
 
-	if request.method == "POST":
-		comment_data = request.POST.dict()
-		comment = comment_data.get("comment_text")
-		# Get the post object
-		package_object = Package.objects.get(pk=package_id)
-		package_object.comments.create(comment=comment, user=request.user)
-		
-	context = package_all_comments(package_id)
-	
-	return render(request, "v2/package/includes/comments.html", context=context)
+    if request.method == "POST":
+        comment_data = request.POST.dict()
+        comment = comment_data.get("comment_text")
+        # Get the post object
+        package_object = Package.objects.get(pk=package_id)
+        package_object.comments.create(comment=comment, user=request.user)
+
+    context = package_all_comments(package_id)
+
+    return render(request, "v2/package/includes/comments.html", context=context)
+
 
 @login_required(login_url="account_login")
 def package_delete_comment(request, link_id):
-	comment = Comment.objects.get(id=link_id)
-	package_id = request.GET.get('package_id')
-	
-	comment.delete()
-	
-	context = package_all_comments(package_id)
-	
-	return render(request, "v2/package/includes/comments.html", context=context)
-	
+    comment = Comment.objects.get(id=link_id)
+    package_id = request.GET.get("package_id")
+
+    comment.delete()
+
+    context = package_all_comments(package_id)
+
+    return render(request, "v2/package/includes/comments.html", context=context)
+
+
 def package_edit_comment(request, link_id):
-	comment = Comment.objects.get(id=link_id)
-	
-	package_id = request.GET.get('package_id')
-	package = Package.objects.get(id=package_id)
-	
-	context = {
-		"comment": comment,
-		"package_object": package,
-	}
-	
-	return render(request, "v2/package/includes/comment_edit.html", context=context)
+    comment = Comment.objects.get(id=link_id)
+
+    package_id = request.GET.get("package_id")
+    package = Package.objects.get(id=package_id)
+
+    context = {
+        "comment": comment,
+        "package_object": package,
+    }
+
+    return render(request, "v2/package/includes/comment_edit.html", context=context)
+
 
 def package_show_comments(request):
-	
-	package_id = request.GET.get('package_id')
-	context = package_all_comments(package_id)
-	
-	return render(request, "v2/package/includes/comments.html", context=context)
+
+    package_id = request.GET.get("package_id")
+    context = package_all_comments(package_id)
+
+    return render(request, "v2/package/includes/comments.html", context=context)
+
 
 def package_update_comment(request, link_id):
-	comment = Comment.objects.get(id=link_id)
-	package_id = request.GET.get('package_id')
-	
-	if request.method == 'POST':
-		comment_data = request.POST.dict()
-		comment_text = comment_data.get("comment_edit_text")
-		comment.comment = comment_text
-		comment.save()
-	
-	context = package_all_comments(package_id)
-	
-	return render(request, "v2/package/includes/comments.html", context=context)
-	
+    comment = Comment.objects.get(id=link_id)
+    package_id = request.GET.get("package_id")
+
+    if request.method == "POST":
+        comment_data = request.POST.dict()
+        comment_text = comment_data.get("comment_edit_text")
+        comment.comment = comment_text
+        comment.save()
+
+    context = package_all_comments(package_id)
+
+    return render(request, "v2/package/includes/comments.html", context=context)
+
+
 def package_all_comments(package_id):
-	
-	package = Package.objects.get(id=package_id)
-	comments = package.comments.all()
-	
-	context = {
-		"comments": comments,
-		"package_object": package,
-	}
-	
-	return context
-	
+
+    package = Package.objects.get(id=package_id)
+    comments = package.comments.all()
+
+    context = {
+        "comments": comments,
+        "package_object": package,
+    }
+
+    return context
+
+
 # ---------------- Package Imagery -------------------------
-	
+
+
 @login_required(login_url="account_login")
 def package_imagery_create_v2(request, link_id):
-	package = Package.objects.get(id=link_id)
-	returnURL = request.GET.get("returnUrl")
+    package = Package.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
 
-	form = PackageImageryForm(initial={"package": package})
-	form_title = "Package Image"
+    form = PackageImageryForm(initial={"package": package})
+    form_title = "Package Image"
 
-	if request.method == "POST":
-		form = PackageImageryForm(request.POST, request.FILES)
-		if form.is_valid():
-			form.save(commit=True)
-			return HttpResponseRedirect(returnURL)
+    if request.method == "POST":
+        form = PackageImageryForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=True)
+            return HttpResponseRedirect(returnURL)
 
-	context = {
-		"form": form,
-		"form_title": form_title,
-		"link": link_id,
-		"returnURL": returnURL,
-	}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
+
 
 @login_required(login_url="account_login")
 def package_imagery_update_v2(request, link_id):
-	imagery = PackageImagery.objects.get(id=link_id)
-	returnURL = request.GET.get("returnUrl")
+    imagery = PackageImagery.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
 
-	form_title = "Package Image"
-	form = PackageImageryForm(instance=imagery)
+    form_title = "Package Image"
+    form = PackageImageryForm(instance=imagery)
 
-	if request.method == "POST":
-		form = PackageImageryForm(request.POST, request.FILES, instance=imagery)
-		print(request.path)
-		if form.is_valid():
-			form.save(commit=True)
-			return HttpResponseRedirect(returnURL)
+    if request.method == "POST":
+        form = PackageImageryForm(request.POST, request.FILES, instance=imagery)
+        print(request.path)
+        if form.is_valid():
+            form.save(commit=True)
+            return HttpResponseRedirect(returnURL)
 
-	context = {
-		"form": form,
-		"form_title": form_title,
-		"link": link_id,
-		"returnURL": returnURL,
-	}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
 
 
 @login_required(login_url="account_login")
 def package_imagery_delete_v2(request, link_id):
-	imagery = PackageImagery.objects.get(id=link_id)
-	returnURL = request.GET.get("returnUrl")
-	
-	# Check to see if an AO Image exists.
-	if imagery:
-		os.remove(os.path.join(settings.MEDIA_ROOT, str(imagery.image)))
-		
-	imagery.delete()
-	return HttpResponseRedirect(returnURL)
+    imagery = PackageImagery.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
+
+    # Check to see if an AO Image exists.
+    if imagery:
+        os.remove(os.path.join(settings.MEDIA_ROOT, str(imagery.image)))
+
+    imagery.delete()
+    return HttpResponseRedirect(returnURL)
