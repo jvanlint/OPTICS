8bd412231cb26ebe0acb5e01550df965fa48bb19
Jason van Lint <jvanlint@mac.com>
2023-10-13 10:53:04 +1100

    Improved flight copy feature to record created and modified by fields.

--- models/Flight.py
+++ models/Flight.py
@@ -3,157 +3,140 @@ from .Target import *
 from django.contrib.contenttypes.fields import GenericRelation
 from .Comment import *
 
+
 class Flight(models.Model):
-	# Fields
+    # Fields
 
-	package = models.ForeignKey(
-		"Package", 
-		on_delete=models.CASCADE, 
-		null=True
-	)
-	callsign = models.CharField(
-		max_length=200, 
-		help_text="Enter Flight Callsign"
-	)
-	task = models.ForeignKey(
-		"Task", 
-		on_delete=models.CASCADE, 
-		null=True
-	)
-	flight_coordination = models.TextField(
-		help_text="Use this field to enter in any notes that the flight lead might need to use to coordinate other members of the flight,",
-		null=True,
-		blank=True,
-		verbose_name="Notes for flight co-ordination",
-	)
-	radio_frequency = models.CharField(
-		max_length=20, 
-		help_text="Enter Flight Frequency", 
-		blank=True, 
-		null=True
-	)
-	tacan = models.CharField(
-		max_length=5,
-		help_text="Enter Flight TACAN (if applicable)",
-		blank=True,
-		null=True,
-	)
-	targets = models.ManyToManyField(
-		Target, 
-		blank=True
-	)
+    package = models.ForeignKey("Package", on_delete=models.CASCADE, null=True)
+    callsign = models.CharField(max_length=200, help_text="Enter Flight Callsign")
+    task = models.ForeignKey("Task", on_delete=models.CASCADE, null=True)
+    flight_coordination = models.TextField(
+        help_text="Use this field to enter in any notes that the flight lead might need to use to coordinate other members of the flight,",
+        null=True,
+        blank=True,
+        verbose_name="Notes for flight co-ordination",
+    )
+    radio_frequency = models.CharField(
+        max_length=20, help_text="Enter Flight Frequency", blank=True, null=True
+    )
+    tacan = models.CharField(
+        max_length=5,
+        help_text="Enter Flight TACAN (if applicable)",
+        blank=True,
+        null=True,
+    )
+    targets = models.ManyToManyField(Target, blank=True)
 
-	# Time Hacks
-	timehack_start = models.CharField(
-		max_length=10,
-		help_text="Enter time for flight takeoff.",
-		null=True,
-		blank=True,
-		verbose_name="Takeoff Time",
-	)
-	timehack_rdv1 = models.CharField(
-		max_length=10,
-		help_text="Enter time for flight rendevous point 1.",
-		null=True,
-		blank=True,
-		verbose_name="Time RDV 1",
-	)
-	timehack_rdv2 = models.CharField(
-		max_length=10,
-		help_text="Enter time for flight rendevous point 2.",
-		null=True,
-		blank=True,
-		verbose_name="Time RDV 2",
-	)
-	fuel_fob = models.CharField(
-		max_length=10,
-		help_text="Fuel FOB",
-		null=True,
-		blank=True,
-		verbose_name="Fuel FOB",
-	)
-	fuel_joker = models.CharField(
-		max_length=10,
-		help_text="Fuel JOKER.",
-		null=True,
-		blank=True,
-		verbose_name="Fuel JOKER",
-	)
-	fuel_bingo = models.CharField(
-		max_length=10,
-		help_text="Fuel BINGO.",
-		null=True,
-		blank=True,
-		verbose_name="Fuel BINGO",
-	)
-	comments = GenericRelation(Comment)
-	created_by = models.ForeignKey(
-		User, 
-		related_name='flight_created_by', 
-		null=True,
-		blank=True,
-		on_delete=models.SET_NULL
-	)
-	date_created = models.DateTimeField(
-		auto_now_add=True, 
-		null=True
-	)
-	date_modified = models.DateTimeField(auto_now=True)
-	modified_by = models.ForeignKey(
-		User, 
-		on_delete=models.SET_NULL, 
-		null=True,
-		blank=True,
-		related_name = 'flight_modified_by'
-	)
-	# Metadata
+    # Time Hacks
+    timehack_start = models.CharField(
+        max_length=10,
+        help_text="Enter time for flight takeoff.",
+        null=True,
+        blank=True,
+        verbose_name="Takeoff Time",
+    )
+    timehack_rdv1 = models.CharField(
+        max_length=10,
+        help_text="Enter time for flight rendevous point 1.",
+        null=True,
+        blank=True,
+        verbose_name="Time RDV 1",
+    )
+    timehack_rdv2 = models.CharField(
+        max_length=10,
+        help_text="Enter time for flight rendevous point 2.",
+        null=True,
+        blank=True,
+        verbose_name="Time RDV 2",
+    )
+    fuel_fob = models.CharField(
+        max_length=10,
+        help_text="Fuel FOB",
+        null=True,
+        blank=True,
+        verbose_name="Fuel FOB",
+    )
+    fuel_joker = models.CharField(
+        max_length=10,
+        help_text="Fuel JOKER.",
+        null=True,
+        blank=True,
+        verbose_name="Fuel JOKER",
+    )
+    fuel_bingo = models.CharField(
+        max_length=10,
+        help_text="Fuel BINGO.",
+        null=True,
+        blank=True,
+        verbose_name="Fuel BINGO",
+    )
+    comments = GenericRelation(Comment)
+    created_by = models.ForeignKey(
+        User,
+        related_name="flight_created_by",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    modified_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="flight_modified_by",
+    )
+    # Metadata
 
-	class Meta:
-		ordering = ["callsign"]
+    class Meta:
+        ordering = ["callsign"]
 
-	# Methods
+    # Methods
 
-	def __str__(self):
-		return self.callsign
-		
-	def new(self, packageObject):
-		new_flight_instance = Flight(
-			package = packageObject,
-			callsign = self.callsign,
-			task = self.task,
-			flight_coordination = self.flight_coordination,
-			radio_frequency = self.radio_frequency,
-			tacan = self.tacan,
-			timehack_start = self.timehack_start,
-			timehack_rdv1 = self.timehack_rdv1,
-			timehack_rdv2 = self.timehack_rdv2,
-			fuel_fob = self.fuel_fob,
-			fuel_joker = self.fuel_joker,
-			fuel_bingo = self.fuel_bingo,
-		)
-		
-		new_flight_instance.save()
-		
-		flight_aircraft = self.aircraft_set.all()
-		if flight_aircraft:
-			for aircraft in flight_aircraft:
-				aircraft.copyToFlight(new_flight_instance)
-		
-		flight_waypoints = self.waypoint_set.all()
-		if flight_waypoints:
-			for waypoint in flight_waypoints:
-				waypoint.copyToFlight(new_flight_instance)
-		
-	def copy(self):
-		packageID = self.package.id
-		
-		self.new(self.package)
-		
-		return packageID
-	
-	def copyToPackage(self, package):
-		packageID = self.package.id
-		
-		self.new(package)
-		
-		return packageID
+    def __str__(self):
+        return self.callsign
+
+    def new(self, packageObject, user):
+        new_flight_instance = Flight(
+            package=packageObject,
+            callsign=self.callsign,
+            task=self.task,
+            flight_coordination=self.flight_coordination,
+            radio_frequency=self.radio_frequency,
+            tacan=self.tacan,
+            timehack_start=self.timehack_start,
+            timehack_rdv1=self.timehack_rdv1,
+            timehack_rdv2=self.timehack_rdv2,
+            fuel_fob=self.fuel_fob,
+            fuel_joker=self.fuel_joker,
+            fuel_bingo=self.fuel_bingo,
+            created_by=user,
+            modified_by=user,
+        )
+
+        new_flight_instance.save()
+
+        flight_aircraft = self.aircraft_set.all()
+        if flight_aircraft:
+            for aircraft in flight_aircraft:
+                aircraft.copyToFlight(new_flight_instance)
+
+        flight_waypoints = self.waypoint_set.all()
+        if flight_waypoints:
+            for waypoint in flight_waypoints:
+                waypoint.copyToFlight(new_flight_instance)
+
+    def copy(self, user):
+        packageID = self.package.id
+
+        self.new(self.package, user)
+
+        return packageID
+
+    def copyToPackage(self, package):
+        packageID = self.package.id
+
+        self.new(package)
+
+        return packageID
--- views/flight_v2.py
+++ views/flight_v2.py
@@ -11,239 +11,268 @@ from django.urls import reverse
 from ..models import Flight, FlightImagery, UserProfile, Comment, Package, Target
 from ..forms import FlightForm, FlightImageryForm
 
+
 @login_required(login_url="account_login")
 def flight_v2(request, link_id):
-	flight = Flight.objects.get(id=link_id)
-	aircraft = flight.aircraft_set.all()
-	waypoints = flight.waypoint_set.all()
-	targets = flight.targets.all()
-	comments = flight.comments.all()
-	imagery = flight.flightimagery_set.all()
-	user_profile = UserProfile.objects.get(user=request.user)
-	
+    flight = Flight.objects.get(id=link_id)
+    aircraft = flight.aircraft_set.all()
+    waypoints = flight.waypoint_set.all()
+    targets = flight.targets.all()
+    comments = flight.comments.all()
+    imagery = flight.flightimagery_set.all()
+    user_profile = UserProfile.objects.get(user=request.user)
 
-	breadcrumbs = {'Home': reverse('campaigns'),  flight.package.mission.campaign.name: reverse('campaign_detail_v2', args=(flight.package.mission.campaign.id,)), flight.package.mission.name: reverse('mission_v2', args=(flight.package.mission.id,)), flight.package.name: reverse('package_v2', args=(flight.package.id,)), flight.callsign:''}
+    breadcrumbs = {
+        "Home": reverse("campaigns"),
+        flight.package.mission.campaign.name: reverse(
+            "campaign_detail_v2", args=(flight.package.mission.campaign.id,)
+        ),
+        flight.package.mission.name: reverse(
+            "mission_v2", args=(flight.package.mission.id,)
+        ),
+        flight.package.name: reverse("package_v2", args=(flight.package.id,)),
+        flight.callsign: "",
+    }
 
-	context = {
-		"flight_object": flight,
-		"aircraft_object": aircraft,
-		"waypoint_object": waypoints,
-		"target_object": targets,
-		"imagery_object": imagery,
-		"isAdmin": user_profile.is_admin(),
-		"comments": comments,
-		"breadcrumbs": breadcrumbs,
-	}
+    context = {
+        "flight_object": flight,
+        "aircraft_object": aircraft,
+        "waypoint_object": waypoints,
+        "target_object": targets,
+        "imagery_object": imagery,
+        "isAdmin": user_profile.is_admin(),
+        "comments": comments,
+        "breadcrumbs": breadcrumbs,
+    }
 
-	return render(request, "v2/flight/flight.html", context=context)
+    return render(request, "v2/flight/flight.html", context=context)
+
 
 @login_required(login_url="account_login")
 def flight_add_v2(request, link_id):
-	package = Package.objects.get(id=link_id)
-	
-	# Filter the target field to just targets from the mission.
-	target = Target.objects.filter(mission=package.mission.id)
-	
-	returnURL = request.GET.get('returnUrl')
+    package = Package.objects.get(id=link_id)
 
-	form_title = 'Flight'
+    # Filter the target field to just targets from the mission.
+    target = Target.objects.filter(mission=package.mission.id)
 
-	form = FlightForm(target, initial={'package': package})
+    returnURL = request.GET.get("returnUrl")
 
-	if request.method == "POST":
-		form = FlightForm(target, request.POST)
-		if form.is_valid():
-			obj=form.save(commit=False)
-			obj.modified_by = request.user
-			obj.created_by = request.user
-			obj.save()
-			return HttpResponseRedirect(returnURL)
+    form_title = "Flight"
 
-	context = {'form': form, 'form_title': form_title,
-			   'link': link_id, 'returnURL': returnURL}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
+    form = FlightForm(target, initial={"package": package})
+
+    if request.method == "POST":
+        form = FlightForm(target, request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.modified_by = request.user
+            obj.created_by = request.user
+            obj.save()
+            return HttpResponseRedirect(returnURL)
+
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
+
 
 @login_required(login_url="account_login")
 def flight_update_v2(request, link_id):
-	flight = Flight.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
-	
-	form_title = 'Flight'
+    flight = Flight.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
 
-	# Filter the target field to just targets from the mission.
-	target = Target.objects.filter(mission=flight.package.mission.id)
+    form_title = "Flight"
 
-	form = FlightForm(target, instance=flight)
+    # Filter the target field to just targets from the mission.
+    target = Target.objects.filter(mission=flight.package.mission.id)
 
-	if request.method == "POST":
-		form = FlightForm(target, request.POST, instance=flight)
-		print(request.path)
-		if form.is_valid():
-			obj=form.save(commit=False)
-			obj.modified_by = request.user
-			obj.save()
-			return HttpResponseRedirect(returnURL)
+    form = FlightForm(target, instance=flight)
 
-	context = {'form': form, 'form_title': form_title,
-			   'link': link_id, 'returnURL': returnURL}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
-	
+    if request.method == "POST":
+        form = FlightForm(target, request.POST, instance=flight)
+        print(request.path)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.modified_by = request.user
+            obj.save()
+            return HttpResponseRedirect(returnURL)
+
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
+
+
 @login_required(login_url="account_login")
 def flight_delete_v2(request, link_id):
-	flight = Flight.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
-	
-	flight.delete()
-	
-	return HttpResponseRedirect(returnURL)
+    flight = Flight.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
+
+    flight.delete()
+
+    return HttpResponseRedirect(returnURL)
+
 
 @login_required(login_url="account_login")
 def flight_copy_v2(request, link_id):
-	flight = Flight.objects.get(id=link_id)
-	returnURL = request.GET.get('returnUrl')
-	packageID = flight.copy()
+    flight = Flight.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
+    packageID = flight.copy(request.user)
 
-	return HttpResponseRedirect(returnURL)
+    return HttpResponseRedirect(returnURL)
 
+
+# Candidate for removal.
 @login_required(login_url="account_login")
 def flight_copy(request, link_id):
-	flight = Flight.objects.get(id=link_id)
-	packageID = flight.copy()
+    flight = Flight.objects.get(id=link_id)
+    packageID = flight.copy()
 
-	return HttpResponseRedirect("/airops/package/" + str(packageID))
-	
+    return HttpResponseRedirect("/airops/package/" + str(packageID))
+
+
 # ---------------- Flight Comments -------------------------
 @login_required(login_url="account_login")
 def flight_add_comment(request):
-	# if this is a POST request we need to process the form data
-	flight_id = request.GET.get("flight_id")
+    # if this is a POST request we need to process the form data
+    flight_id = request.GET.get("flight_id")
 
-	if request.method == "POST":
-		comment_data = request.POST.dict()
-		comment = comment_data.get("comment_text")
-		# Get the post object
-		flight_object = Flight.objects.get(pk=flight_id)
-		flight_object.comments.create(comment=comment, user=request.user)
-		
+    if request.method == "POST":
+        comment_data = request.POST.dict()
+        comment = comment_data.get("comment_text")
+        # Get the post object
+        flight_object = Flight.objects.get(pk=flight_id)
+        flight_object.comments.create(comment=comment, user=request.user)
 
-	context = flight_all_comments(flight_id)
-	
-	return render(request, "v2/flight/includes/comments.html", context=context)
+    context = flight_all_comments(flight_id)
+
+    return render(request, "v2/flight/includes/comments.html", context=context)
+
 
 @login_required(login_url="account_login")
 def flight_delete_comment(request, link_id):
-	flight_id = request.GET.get('flight_id')
-	comment = Comment.objects.get(id=link_id)
-	
-	comment.delete()
-	
-	context = flight_all_comments(flight_id)
-	
-	return render(request, "v2/flight/includes/comments.html", context=context)
-	
+    flight_id = request.GET.get("flight_id")
+    comment = Comment.objects.get(id=link_id)
+
+    comment.delete()
+
+    context = flight_all_comments(flight_id)
+
+    return render(request, "v2/flight/includes/comments.html", context=context)
+
+
 def flight_edit_comment(request, link_id):
-	comment = Comment.objects.get(id=link_id)
-	
-	flight_id = request.GET.get('flight_id')
-	flight = Flight.objects.get(id=flight_id)
-	
-	context = {
-		"comment": comment,
-		"flight_object": flight,
-	}
-	
-	return render(request, "v2/flight/includes/comment_edit.html", context=context)
+    comment = Comment.objects.get(id=link_id)
+
+    flight_id = request.GET.get("flight_id")
+    flight = Flight.objects.get(id=flight_id)
+
+    context = {
+        "comment": comment,
+        "flight_object": flight,
+    }
+
+    return render(request, "v2/flight/includes/comment_edit.html", context=context)
+
 
 def flight_show_comments(request):
-	
-	flight_id = request.GET.get('flight_id')
-	context = flight_all_comments(flight_id)
-	
-	return render(request, "v2/flight/includes/comments.html", context=context)
+    flight_id = request.GET.get("flight_id")
+    context = flight_all_comments(flight_id)
+
+    return render(request, "v2/flight/includes/comments.html", context=context)
+
 
 def flight_update_comment(request, link_id):
-	comment = Comment.objects.get(id=link_id)
-	flight_id = request.GET.get('flight_id')
-	
-	if request.method == 'POST':
-		comment_data = request.POST.dict()
-		comment_text = comment_data.get("comment_edit_text")
-		comment.comment = comment_text
-		comment.save()
-	
-	context = flight_all_comments(flight_id)
-	
-	return render(request, "v2/flight/includes/comments.html", context=context)
+    comment = Comment.objects.get(id=link_id)
+    flight_id = request.GET.get("flight_id")
+
+    if request.method == "POST":
+        comment_data = request.POST.dict()
+        comment_text = comment_data.get("comment_edit_text")
+        comment.comment = comment_text
+        comment.save()
+
+    context = flight_all_comments(flight_id)
+
+    return render(request, "v2/flight/includes/comments.html", context=context)
+
 
 def flight_all_comments(flight_id):
-	
-	flight = Flight.objects.get(id=flight_id)
-	comments = flight.comments.all()
-	
-	context = {
-		"comments": comments,
-		"flight_object": flight,
-	}
-	
-	return context
-	
+    flight = Flight.objects.get(id=flight_id)
+    comments = flight.comments.all()
+
+    context = {
+        "comments": comments,
+        "flight_object": flight,
+    }
+
+    return context
+
+
 # ---------------- Flight Imagery -------------------------
-	
+
+
 @login_required(login_url="account_login")
 def flight_imagery_create_v2(request, link_id):
-	flight = Flight.objects.get(id=link_id)
-	returnURL = request.GET.get("returnUrl")
+    flight = Flight.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
 
-	form = FlightImageryForm(initial={"flight": flight})
-	form_title = "Flight Image"
+    form = FlightImageryForm(initial={"flight": flight})
+    form_title = "Flight Image"
 
-	if request.method == "POST":
-		form = FlightImageryForm(request.POST, request.FILES)
-		if form.is_valid():
-			form.save(commit=True)
-			return HttpResponseRedirect(returnURL)
+    if request.method == "POST":
+        form = FlightImageryForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=True)
+            return HttpResponseRedirect(returnURL)
 
-	context = {
-		"form": form,
-		"form_title": form_title,
-		"link": link_id,
-		"returnURL": returnURL,
-	}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
 
 
 @login_required(login_url="account_login")
 def flight_imagery_update_v2(request, link_id):
-	imagery = FlightImagery.objects.get(id=link_id)
-	returnURL = request.GET.get("returnUrl")
+    imagery = FlightImagery.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
 
-	form_title = "Flight Image"
-	form = FlightImageryForm(instance=imagery)
+    form_title = "Flight Image"
+    form = FlightImageryForm(instance=imagery)
 
-	if request.method == "POST":
-		form = FlightImageryForm(request.POST, request.FILES, instance=imagery)
-		print(request.path)
-		if form.is_valid():
-			form.save(commit=True)
-			return HttpResponseRedirect(returnURL)
+    if request.method == "POST":
+        form = FlightImageryForm(request.POST, request.FILES, instance=imagery)
+        print(request.path)
+        if form.is_valid():
+            form.save(commit=True)
+            return HttpResponseRedirect(returnURL)
 
-	context = {
-		"form": form,
-		"form_title": form_title,
-		"link": link_id,
-		"returnURL": returnURL,
-	}
-	return render(request, 'v2/generic/data_entry_form.html', context=context)
+    context = {
+        "form": form,
+        "form_title": form_title,
+        "link": link_id,
+        "returnURL": returnURL,
+    }
+    return render(request, "v2/generic/data_entry_form.html", context=context)
 
 
 @login_required(login_url="account_login")
 def flight_imagery_delete_v2(request, link_id):
-	imagery = FlightImagery.objects.get(id=link_id)
-	returnURL = request.GET.get("returnUrl")
-	
-	# Check to see if an AO Image exists.
-	if imagery:
-		os.remove(os.path.join(settings.MEDIA_ROOT, str(imagery.image)))
-		
-	imagery.delete()
-	return HttpResponseRedirect(returnURL)
+    imagery = FlightImagery.objects.get(id=link_id)
+    returnURL = request.GET.get("returnUrl")
+
+    # Check to see if an AO Image exists.
+    if imagery:
+        os.remove(os.path.join(settings.MEDIA_ROOT, str(imagery.image)))
+
+    imagery.delete()
+    return HttpResponseRedirect(returnURL)
